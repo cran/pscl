5da6225784bfba0cb1e07aa4c03e5c77415b9160
Simon Jackman <jackman@stanford.edu>
2015-03-29 00:00:00 +0000

    version 1.4.9

--- R/vuong.R
+++ R/vuong.R
@@ -18,45 +18,61 @@
     stop(paste("Models appear to have different values on dependent variables.\n"))
   }
   
-  whichCol <- match(m1y,min(m1y):max(m1y))  ## which column, matrix of predicted probs
-  
-  m1p <- rep(NA,m1n)
-  m2p <- rep(NA,m2n)
   p1 <- predprob(m1)   ## likelihood contributions, model 1, cond on MLEs
   p2 <- predprob(m2)   ## likelihood contributions, model 2
+  if(!all(colnames(p1)==colnames(p2))){
+    stop("Models appear to have different values on dependent variables.\n")
+  }
+  
+
+  whichCol <- match(m1y,colnames(p1))  ## which column, matrix of predicted probs
+  whichCol2 <- match(m2y,colnames(p2))
+  if(!all(whichCol==whichCol2)){
+    stop("Models appear to have different values on dependent variables.\n")
+  }
+                     
+  m1p <- rep(NA,m1n)
+  m2p <- rep(NA,m2n)
   for(i in 1:m1n){
-    m1p[i] <- p1[i,whichCol[i]]  ## pick off correct column, given observed y
+    m1p[i] <- p1[i,whichCol[i]]  ## pick off correct predicted probabilty, for observed y
     m2p[i] <- p2[i,whichCol[i]]
   }
+  
   
   ## gather up degrees of freedom
   k1 <- length(coef(m1))
   k2 <- length(coef(m2))
   
-  m <- log(m1p) - log(m2p)  ## vector of log likelihood ratios (diffs of log probabilities)
+  lm1p <- log(m1p)
+  lm2p <- log(m2p)
+  m <- lm1p - lm2p  ## vector of log likelihood ratios (diffs of log probabilities)
   
-  bad <- is.na(m) | is.nan(m) | is.infinite(m)
+  bad1 <- is.na(lm1p) | is.nan(lm1p) | is.infinite(lm1p)
+  bad2 <- is.na(lm2p) | is.nan(lm2p) | is.infinite(lm2p)
+  bad3   <- is.na(m) | is.nan(m) | is.infinite(m)
+  bad <- bad1 | bad2 | bad3
   neff <- sum(!bad)
+
   if(any(bad)){
     cat("NA or numerical zeros or ones encountered in fitted probabilities\n")
     cat(paste("dropping these",sum(bad),"cases, but proceed with caution\n"))
   }
   
-  aic.factor <- (k1-k2)/neff          
-  bic.factor <- (k1-k2)/2 * log(neff)   
-  print(bic.factor)
+  aic.factor <- (k1-k2)/neff        
+  bic.factor <- (k1-k2)/(2*neff) * log(neff)   
   
-  ## test statistics
+  ## 3 test statistics
   v <- rep(NA,3)
-  L1 <- sum(log(m1p[!bad]))
-  L2 <- sum(log(m2p[!bad]))
-  num <- rep(L1-L2,3) - c(0,aic.factor,bic.factor) 
-  s <- sd(m[!bad])
-  v <- num/(s*sqrt(neff))  
+  arg1 <- matrix(m[!bad],nrow=neff,ncol=3,byrow=FALSE)
+  arg2 <- matrix(c(0,aic.factor,bic.factor),nrow=neff,ncol=3,byrow=TRUE)
+  num <- arg1 - arg2
+  s <- apply(num,2,sd)
+  numsum <- apply(num,2,sum)
+  v <- numsum/(s*sqrt(neff))     ## Vuong  
   names(v) <- c("Raw","AIC-corrected","BIC-corrected")
-  print(v)
-  print(s)
-  print(num)
+  ##print(v)
+  ##print(s)
+  ##print(num)
   
   ## bundle up for output
   pval <- rep(NA,3)
